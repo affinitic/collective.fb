edc3f66cc62e670c9186a1d35c6d7fed9f42c44d
RedTurtle <redturtle@redturtle.local>
2012-10-13 17:53:31 +0200

    cleanup

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ version = '1.0'
 
 setup(name='collective.fb',
       version=version,
-      description="Plone Developer Documentation",
+      description="Plone Facebook integration bundle",
+      long_description=open("README.rst").read(),
       classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
@@ -17,13 +18,16 @@ setup(name='collective.fb',
       keywords='',
       author='Massimo Azzolini, Mauro Amico, Andrzej Mleczko',
       author_email='sviluppoplone@redturtle.it',
-      url='http://plone.org',
+      url='http://plone.org/products/collective.fb',
       license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['collective'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
           'setuptools',
+          'collective.opengraph',
           # -*- Extra requirements: -*-
       ],
       entry_points="""
